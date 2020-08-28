4ddb9f81dcdc45c6ab20ca6500db464e624c1a2f
mdseg <marcos.seghesio@gmail.com>
2020-08-27 22:05:37 -0300

    Commit de prueba

---
+++ TP1/src/TP1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TP1.c
+ Author      : Marcos Seghesio
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hola!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
